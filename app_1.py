a313b368ba6fe489fb48a022396c97540cf4ccaa
Makram <131681631+makramkb@users.noreply.github.com>
2024-05-31 00:38:32 -0400

    Add files via upload

---
+++ app_1.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify 
+
+ 
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine,inspect
+
+base = automap_base()
+
+engine = create_engine(f'sqlite:///chinook.sqlite')
+base.prepare(autoload_with=engine)
+session = Session(bind=engine)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    tables_name = base.classes.keys()
+    return tables_name
+
+@app.route('/column_names')
+
+def col_names():
+
+    inspector = inspect(engine)
+    tabels_info = {}
+    table_names = base.classes.keys()
+
+    for table_name in table_names:
+        column_names = inspector.get_columns(table_name)
+
+        col_names = [col['name'] for col in column_names]
+
+        tabels_info[table_name] = col_names
+
+    return jsonify(tabels_info)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
